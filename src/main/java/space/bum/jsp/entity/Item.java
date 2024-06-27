98aa999b1d0488b05824dc27fa6cb175a0cccc2c
osparking <jbpark03@gmail.com>
2024-06-28 06:26:02 +0900

    price 는 null 값을 가질 수 없다

--- src/main/java/space/bum/jsp/entity/Item.java
+++ src/main/java/space/bum/jsp/entity/Item.java
@@ -5,13 +5,17 @@ import java.math.BigDecimal;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 @Entity
+@Data
 public class Item {
 	@Id
 	@GeneratedValue
 	private Long id;
 	
+	@NotNull
 	private BigDecimal price;
 
 }
